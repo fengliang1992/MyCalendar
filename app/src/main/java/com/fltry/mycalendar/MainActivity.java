f08b9bf80d183e25868eaee94bcb46dd699e8bf5
fengliang <732419012@qq.com>
2019-01-04 09:27:39 +0800

    日历

---
+++ app/src/main/java/com/fltry/mycalendar/MainActivity.java
@@ -0,0 +1,45 @@
+package com.fltry.mycalendar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    MyCalendar myCalendar;
+    String[][] colors;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myCalendar = findViewById(R.id.myCalendar);
+        colors = new String[][]{{"2019-1-3", "#ffffff", "#fda2a1"},
+                {"2019-1-6", "#ffffff", "#a2c4fc"},
+                {"2019-1-9", "#ffffff", "#a2c4fc"},
+                {"2019-1-12", "#ffffff", "#a2c4fc"},
+                {"2019-1-20", "#ffffff", "#fcbd7f"},
+                {"2019-1-29", "#ffffff", "#fcbd7f"},
+                {"2019-1-31", "#ffffff", "#fcbd7f"},
+                {"2019-2-5", "#ffffff", "#fcbd7f"},
+                {"2019-5-6", "#ffffff", "#a2c4fc"},
+                {"2019-3-21", "#ffffff", "#fcbd7f"},
+                {"2019-4-4", "#ffffff", "#a2c4fc"},
+                {"2019-8-8", "#ffffff", "#fcbd7f"}
+        };
+        myCalendar.setDayColor(colors);
+
+
+        myCalendar.setOnSelectClickListener(new MyCalendar.OnSelectClickListener() {
+            @Override
+            public void onSelect(String year, String month, String day) {
+
+            }
+
+            @Override
+            public void onMonthChange(String year, String month) {
+                myCalendar.setDayColor(colors);
+            }
+        });
+    }
+}
